04f08120aff5aff92cbc03e5abb2ce584a920021
laura chan <laura.chan.2010@gmail.com>
2020-03-04 22:45:29 -0500

    num_pages, output are optional parameters

--- main.py
+++ main.py
@@ -5,38 +5,60 @@ from os import environ
 if __name__ == "__main__":
     app_key = environ.get("APP_KEY")
     # print("APP_KEY={}".format(app_key))
-    page_size_str = argv[1]
-    page_size = int(page_size_str.split('=')[1])
-    num_pages_str = argv[2]
-    num_pages = int(num_pages_str.split('=')[1])
-    # print(f"page_size={page_size}, num_pages={num_pages}")
-    try:
+    page_size = None
+    num_pages = None
+    output = None
+
+    if len(argv) >= 4:
+        page_size_str = argv[1]
+        page_size = int(page_size_str.split('=')[1])
+        num_pages_str = argv[2]
+        num_pages = int(num_pages_str.split('=')[1])
         output_str = argv[3]
-    except Exception:
-        output_str = None
+        output = output_str.split('==')[1]
+    elif len(argv) == 3:
+        page_size_str = argv[1]
+        page_size = int(page_size_str.split('=')[1])
+        if argv[2].split('==')[0] == '--num_pages':
+            num_pages = int(argv[2].split('==')[1])
+        else:
+            output = argv[2].split('==')[1]
+    elif len(argv) == 2:
+        page_size_str = argv[1]
+        page_size = int(page_size_str.split('=')[1])
+    else:
+        raise Exception("Must provide parameter: --page_size=? ")
+
+    # print(f"page_size={page_size}, num_pages={num_pages}")
+
     location = 'nc67-uf89'
 
     limit_size = int(page_size / num_pages)
 
-    if output_str is None:
+    if output is None:
         with Service(app_key) as service:
-            total_size = service.get_size(location)
-            # print(f"total_size={total_size}")
-            print(service.get_info(location, limit_size))
-            offset = 0
-            for i in range(num_pages-1):
-                offset += limit_size
-                if offset >= total_size:
-                    break;
-                print(service.get_next_info(location, limit_size, offset))
+            if num_pages is None:
+                print(service.get_info(location, limit_size))
+            else:
+                total_size = service.get_size(location)
+                # print(f"total_size={total_size}")
+                print(service.get_info(location, limit_size))
+                offset = 0
+                for i in range(num_pages-1):
+                    offset += limit_size
+                    if offset >= total_size:
+                        break;
+                    print(service.get_next_info(location, limit_size, offset))
     else:
-        output = output_str.split("=")[1]
         with Service(app_key) as service, open(output, "w") as fw:
-            total_size = service.get_size(location)
-            fw.write(f"{service.get_info(location, limit_size)}\n")
-            offset = 0
-            for i in range(num_pages-1):
-                offset += limit_size
-                if offset >= total_size:
-                    break;
-                fw.write(f"{service.get_next_info(location, limit_size, offset)}\n")
+            if num_pages is None:
+                fw.write(f"{service.get_info(location, limit_size)}\n")
+            else:
+                total_size = service.get_size(location)
+                fw.write(f"{service.get_info(location, limit_size)}\n")
+                offset = 0
+                for i in range(num_pages-1):
+                    offset += limit_size
+                    if offset >= total_size:
+                        break;
+                    fw.write(f"{service.get_next_info(location, limit_size, offset)}\n")
